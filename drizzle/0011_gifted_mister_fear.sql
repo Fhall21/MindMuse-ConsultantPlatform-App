37e8c7d0e574803571ac59a95289d460cafef7a6
Fhall21 <felix.p.hall@gmail.com>
2026-03-22 22:28:11 +1000

    chore: remove 0011 migration files - will regenerate as 0012 after syncing main

--- drizzle/0011_gifted_mister_fear.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "ocr_jobs" ADD COLUMN "batch_id" uuid;--> statement-breakpoint
-ALTER TABLE "ocr_jobs" ADD COLUMN "image_sequence" integer;--> statement-breakpoint
-CREATE INDEX "idx_ocr_jobs_batch_sequence" ON "ocr_jobs" USING btree ("batch_id","image_sequence");
